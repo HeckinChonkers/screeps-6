cc48428dea6c4f99f93daceb6cabb7f97276f471
InherentVice <treeheadw@gmail.com>
2016-06-30 00:13:30 +0200

    Builder status

--- src/role.builder.js
+++ src/role.builder.js
@@ -15,20 +15,23 @@ var roleBuilder = {
 
   /** @param {Creep} creep **/
   run: function(creep) {
-    creep.memory.currentTask = 'build';
+
 
     utils.cFullCheck(creep);
 
     if (creep.memory.fullCheck) {
       var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
       if (targets.length) {
+        creep.memory.currentTask = 'build';
         if (creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
           creep.moveTo(targets[0]);
         }
       } else {
+        creep.memory.currentTask = 'repair';
         utils.cRepair(creep);
       }
     } else {
+      creep.memory.currentTask = 'refill';
       if (Boolean(creep.room.storage) && creep.room.storage.energy >= creep.carryCapacity) {
         if (creep.room.storage.transfer(creep, 'energy') === ERR_NOT_IN_RANGE) {
           creep.moveTo(creep.room.storage);
